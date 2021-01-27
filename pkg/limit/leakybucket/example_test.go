485055cde45ef6936e12dbb292fd86aba2e48f37
Allenxuxu <120582243@qq.com>
2021-01-27 22:15:08 +0800

    update leaky bucket limiter example test

--- pkg/limit/leakybucket/example_test.go
+++ pkg/limit/leakybucket/example_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ExampleLeakyBucketRateLimitTake() {
-	rl := New(100) // per second
+	rl := New(10) // per second
 
 	prev := time.Now()
 	for i := 0; i < 10; i++ {
@@ -20,15 +20,15 @@ func ExampleLeakyBucketRateLimitTake() {
 	}
 
 	// Output:
-	// 1 10
-	// 2 10
-	// 3 10
-	// 4 10
-	// 5 10
-	// 6 10
-	// 7 10
-	// 8 10
-	// 9 10
+	// 1 100
+	// 2 100
+	// 3 100
+	// 4 100
+	// 5 100
+	// 6 100
+	// 7 100
+	// 8 100
+	// 9 100
 }
 
 func ExampleLeakyBucketRateLimitAllow() {
